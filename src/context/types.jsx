29465942c64370761347b1f8699a2ca068cd8d99
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-03 15:29:56 +0530

    state types

---
+++ src/context/types.jsx
@@ -0,0 +1,4 @@
+export const GET_MOVIES = 'GET_MOVIES';
+export const GET_FAVORITES = 'GET_FAVORITES';
+export const ADD_FAVORITES = 'ADD_FAVORITES';
+export const REMOVE_FAVORITES = 'REMOVE_FAVORITES';

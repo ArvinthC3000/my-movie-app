38a83025cc3ab60f9a583ea280bfcd4ac75d8714
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-03 20:07:27 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 import Movies from './components/Movies';
 import MovieState from './context/movies/MoviesState';
 
 const App = () => {
   return (
     <MovieState>
+      <Header />
       <Movies />
     </MovieState>
   );

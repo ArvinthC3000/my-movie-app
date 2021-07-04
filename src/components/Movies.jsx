17e6dd0f4b794171c1ff2b08f0e896008b98bae5
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-04 15:51:56 +0530

    get default movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,19 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import MoviesList from './MoviesList';
 import MoviesContext from './../context/movies/movieContext';
 
 const Movies = () => {
   const movieContext = useContext(MoviesContext);
-  const { movies, defaultMovies } = movieContext;
+  const { movies, defaultMovies, getDefaultMovies } = movieContext;
+  useEffect(() => {
+    getDefaultMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
       <div className='header'>Popular</div>
       <div className='movieContainer'>
         {movies.length
-          ? movies.map(movie => <MoviesList key={movie.imdbID} movie={movie} />)
+          ? movies.map(movie => <MoviesList key={movie.id} movie={movie} />)
           : defaultMovies.map(movie => (
-              <MoviesList key={movie.imdbID} movie={movie} />
+              <MoviesList key={movie.id} movie={movie} />
             ))}
       </div>
     </>

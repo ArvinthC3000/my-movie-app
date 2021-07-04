21f5299c20db7096d9c7aa890e2b0878ec8fe500
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-04 15:54:22 +0530

    Search bar state changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,22 @@
+import { useContext } from 'react';
+import MoviesContext from './../context/movies/movieContext';
+
 const SearchBar = () => {
+  const movieContext = useContext(MoviesContext);
+  const { getMovies } = movieContext;
+  const searchMovies = e => {
+    getMovies(e.target.value);
+  };
+
   return (
     <>
       <div className='searchBar'>
-        <input type='text' name='movieString' placeholder='Search movies...' />
+        <input
+          type='text'
+          onChange={searchMovies}
+          name='movieString'
+          placeholder='Search movies...'
+        />
       </div>
     </>
   );

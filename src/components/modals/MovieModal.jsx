d71513a10d366a4d030875a42a7d304b7e319ac5
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-07 22:30:51 +0530

    Modal css

--- src/components/modals/MovieModal.jsx
+++ src/components/modals/MovieModal.jsx
@@ -2,6 +2,11 @@ const MovieModal = () => {
   return (
     <div id='movie-modal' className='modal' style={modalStyle}>
       <div className='modal-content'>This is a modal</div>
+      <div className='modal-footer'>
+        <a href='#!' className='modal-close waves-effect blue waves-light btn'>
+          Enter
+        </a>
+      </div>
     </div>
   );
 };
@@ -9,6 +14,9 @@ const MovieModal = () => {
 const modalStyle = {
   width: '75%',
   height: '75%',
+  zIndex: 5,
+  transform: 'translate(0px, -50%)',
+  top: '50%',
 };
 
 export default MovieModal;

73aa44e966590a44c3453f68d49f1847b0c5fdaf
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-11 14:15:16 +0530

    adding root provider to app js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import About from './components/About';
 import Home from './components/Home';
 import MovieModal from './components/modals/MovieModal';
 import MovieState from './context/movies/MoviesState';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   useEffect(() => {
@@ -17,15 +19,15 @@ const App = () => {
   });
 
   return (
-    <MovieState>
-      <MovieModal />
+    <Provider store={store}>
+      {/* <MovieModal /> */}
       <Router>
         <Switch>
-          <Route path='/' exact component={Home} />
+          {/* <Route path='/' exact component={Home} /> */}
           <Route path='/about' exact component={About} />
         </Switch>
       </Router>
-    </MovieState>
+    </Provider>
   );
 };
 
